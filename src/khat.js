4e49f6ef226731475f0a9bdb134cf69a423b51cf
brosef <miles@lumoslabs.com>
2014-04-01 21:20:52 -0700

    fix typo

--- src/khat.js
+++ src/khat.js
@@ -1,6 +1,6 @@
 var express = require('express'), app = express.createServer();
 
-app.set('views', __dirname + 'views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false});
 app.configure(function() {
