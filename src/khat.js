d6c4265dd9643c9c6f48d986021f2f40cdf56c08
brosef <miles@lumoslabs.com>
2014-04-01 21:48:18 -0700

    add server-side socket.io

--- src/khat.js
+++ src/khat.js
@@ -1,5 +1,6 @@
 var express = require('express'), app = express.createServer();
 var jade = require('jade');
+var io = require('socket.io').listen(app)
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -12,3 +13,18 @@ app.get('/', function(req, res){
   res.render('home.jade');
 });
 app.listen(4000);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('setPseudo', function (data) {
+        socket.et('pseudo', data);
+    });
+    socket.on('message', function (message) {
+        socket.get('pseudo', function (error, name) {
+            var data = { 'message' : message, pseudo : name };
+            socket.broadcast.emit('message', data);
+            console.log("user " + name " send this: " + message);
+        })
+    });
+});
+
+
