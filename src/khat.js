d502527f3763d1ee8a3a4b2db384f99194f3d01b
brosef <miles@lumoslabs.com>
2014-04-01 21:19:27 -0700

    khat is a GO

---
+++ src/khat.js
@@ -0,0 +1,13 @@
+var express = require('express'), app = express.createServer();
+
+app.set('views', __dirname + 'views');
+app.set('view engine', 'jade');
+app.set("view options", { layout: false});
+app.configure(function() {
+    app.use(express.static(__dirname + '/public'))
+});
+
+app.get('/', function(req, res){
+  res.render('home.jade');
+});
+app.listen(4000);
