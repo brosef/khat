1c56f1f3c6af98997577a8088cc8b3c3b9a09fa0
Miles Monteleone <scratchyseal@gmail.com>
2014-05-31 23:45:32 -0700

    here it is working. could've used more frequent commits, but whatever

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var http = require('http'),
-var express = require('express'),
-	app = express(),
-var server = http.createServer(app).listen(3000),
-var jade = require('jade'),
-var io = require('socket.io').listen(server)
+var http = require('http')
+,	express = require('express')
+,	app = express()
+,	server = http.createServer(app).listen(3000)
+,	jade = require('jade')
+,	io = require('socket.io').listen(server)
 ;
 
-app.set('views', __dirname + 'views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false });
 app.configure(function() {
@@ -16,5 +16,22 @@ app.configure(function() {
 app.get('/', function(req, res) {
 	res.render('home.jade');
 });
-app.listen('3000');
+
+io.sockets.on('connection', function(socket) {
+
+	socket.on('setPseudo', function(data) {
+		console.log("setPseudo is passing " + data + " to the server");
+		socket.set('pseudo', data);
+	});
+
+	socket.on('message', function(message) {
+		socket.get('pseudo', function(error, name) {
+			var data = { 'message' : message, 'pseudo' : name };
+			console.log(data);
+			socket.broadcast.emit('message', data);
+			console.log("user " + name + " send this : " + message);
+		})
+	});
+
+});
 
